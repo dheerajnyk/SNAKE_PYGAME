7e38ee2353febf20ab3001d07ce18df0f95ffae1
dheerajnyk <dheerajnayak1995@gmail.com>
2018-01-28 13:13:34 +0530

    added snake head and minor changes

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
 lol=(45,78,52)
-
+img=pygame.image.load('snakehead.png')
 display_width=800
 display_height=600
 
@@ -18,32 +18,49 @@ pygame.display.set_caption('slither')
 
 
 clock=pygame.time.Clock()
-block_size=10
-fps=30
+block_size=20
+fps=15
+direction="right"
 font=pygame.font.SysFont(None,25)
 def snake(block_size,snakelist):
-    for XnY in snakelist:
+    if(direction=="right"):
+        head=pygame.transform.rotate(img,270)
+    if(direction=="left"):
+        head=pygame.transform.rotate(img,90)
+    if(direction=="up"):
+        head=img
+    if(direction=="down"):
+        head=pygame.transform.rotate(img,180)
+    
+    
+    gameDisplay.blit(head,(snakelist[-1][0],snakelist[-1][1]))
+    for XnY in snakelist[:-1]:
         pygame.draw.rect(gameDisplay,green,[XnY[0],XnY[1],block_size,block_size])
         
+def text_objects(text,color):
+    textsurface=font.render(text,True,color)
+    return textsurface,textsurface.get_rect()
 
     
 
 
 
-def message(msg,color):
-    screen_text=font.render(msg,True,color)
-    gameDisplay.blit(screen_text,[display_width/2,display_height/2])
+def message(msg,color,y_displace=0):
+   textsurf,textrect=text_objects(msg,color)
+   textrect.center=(0),(0)+y_displace
+   #textrect.center=(display_width/2),(display_height/2)+y_displace
+   gameDisplay.blit(textsurf,textrect)
        
 
 def gameloop():
     snakelist=[]
-    
+    global direction
 
     snakelength=1
     gameover=False
     gameExit= False
     lead_x=display_width/2
-    lead_x_change=0
+    lead_x_change=10
     lead_y_change=0
     lead_y=display_height/2
     randapplex=round(random.randint(0,display_width-block_size)/10)*10
@@ -52,14 +69,16 @@ def gameloop():
     while not gameExit:
         while gameover== True:
             gameDisplay.fill(white)
-            message("press Y or N for continue",red)
+            message("GAME OVER",red,+50)
+            message("Press Y to play again and N to quit",black)
             pygame.display.update()
+            
             for event in pygame.event.get():
                 if event.type==pygame.KEYDOWN:
                     if event.key== pygame.K_n:
                         gameExit=True
                         gameover=False
-                        
+
                     elif event.key== pygame.K_y:
                         gameloop()
                     
@@ -70,15 +89,19 @@ def gameloop():
                 if event.key==pygame.K_LEFT:
                     lead_x_change=-block_size
                     lead_y_change=0
+                    direction="left"
                 elif event.key==pygame.K_RIGHT:
                     lead_x_change=block_size
                     lead_y_change=0
+                    direction="right"
                 elif event.key==pygame.K_DOWN:
                     lead_y_change=+block_size
                     lead_x_change=0
+                    direction="down"
                 elif event.key==pygame.K_UP:
                     lead_y_change=-block_size
                     lead_x_change=0
+                    direction="up"
         if lead_x>=display_width or lead_x<= 0 or lead_y>=display_height or lead_y<=0:
             gameover=True
                 
@@ -90,6 +113,7 @@ def gameloop():
         lead_x+=lead_x_change
         lead_y+=lead_y_change
         gameDisplay.fill(white)
+        applethickness=30
         pygame.draw.rect(gameDisplay,red,[randapplex,randappley,block_size,block_size])
         
         snakehead=[]
@@ -114,13 +138,22 @@ def gameloop():
 
         pygame.display.update()
 
-        if(lead_x==randapplex and lead_y==randappley):
-            randapplex=round(random.randint(0,display_width-block_size)/10)*10
-    
-            randappley=round(random.randint(0,display_height-block_size)/10)*10
-            snakelength+=1
+        if lead_x > randapplex and lead_x<randapplex+applethickness or lead_x+block_size>randapplex and lead_x+block_size<randapplex+applethickness:
             
+            if lead_y > randappley and lead_y<randappley+applethickness or lead_y+block_size>randappley and lead_y+block_size<randappley+applethickness:
+                randapplex=round(random.randint(0,display_width-block_size)/10)*10
+                randappley=round(random.randint(0,display_height-block_size)/10)*10
+                snakelength+=1
+            
+        
       
+
+
+
+
+
+
+
         clock.tick(fps)
             
             
@@ -132,8 +165,7 @@ def gameloop():
 
 
 
-    message("you suck",red)
-    
+    message("NEVER SAY QUIT !!!!",black)
     pygame.display.update()
     time.sleep(2)
     pygame.quit()
